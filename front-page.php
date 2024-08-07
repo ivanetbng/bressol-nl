2835186d94f4bd9d854499cf19d4feff70d67740
Ivan <ivanetbng@gmail.com>
2024-08-07 12:16:34 +0200

    Added front-page template and initial styles for home page

--- front-page.php
+++ front-page.php
@@ -5,7 +5,7 @@ get_header();
 <div id="primary" class="content-area">
     <main id="main" class="site-main">
         <!-- Aquí irá el contenido de tu página principal -->
-         
+        <h1>Bienvenido a Bressol NL</h1>
     </main><!-- #main -->
 </div><!-- #primary -->
 
