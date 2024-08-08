e09dd894e237a233f5cea4f95cdc0e98681de7b8
Ivan <ivanetbng@gmail.com>
2024-08-08 11:59:16 +0200

    Styled archive-product page with Bootstrap and added custom styles for add-to-cart button

--- functions.php
+++ functions.php
@@ -8,177 +8,105 @@
  */
 
 if ( ! defined( '_S_VERSION' ) ) {
-	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+    // Replace the version number of the theme on each release.
+    define( '_S_VERSION', '1.0.0' );
 }
 
 /**
  * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
  */
 function bressol_nl_setup() {
-	/*
-		* Make theme available for translation.
-		* Translations can be filed in the /languages/ directory.
-		* If you're building a theme based on bressol.nl, use a find and replace
-		* to change 'bressol-nl' to the name of your theme in all the template files.
-		*/
-	load_theme_textdomain( 'bressol-nl', get_template_directory() . '/languages' );
+    load_theme_textdomain( 'bressol-nl', get_template_directory() . '/languages' );
 
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+    add_theme_support( 'automatic-feed-links' );
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'post-thumbnails' );
 
-	/*
-		* Let WordPress manage the document title.
-		* By adding theme support, we declare that this theme does not use a
-		* hard-coded <title> tag in the document head, and expect WordPress to
-		* provide it for us.
-		*/
-	add_theme_support( 'title-tag' );
+    register_nav_menus(
+        array(
+            'menu-1' => esc_html__( 'Primary', 'bressol-nl' ),
+        )
+    );
 
-	/*
-		* Enable support for Post Thumbnails on posts and pages.
-		*
-		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		*/
-	add_theme_support( 'post-thumbnails' );
+    add_theme_support(
+        'html5',
+        array(
+            'search-form',
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+            'style',
+            'script',
+        )
+    );
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus(
-		array(
-			'menu-1' => esc_html__( 'Primary', 'bressol-nl' ),
-		)
-	);
+    add_theme_support(
+        'custom-background',
+        apply_filters(
+            'bressol_nl_custom_background_args',
+            array(
+                'default-color' => 'ffffff',
+                'default-image' => '',
+            )
+        )
+    );
 
-	/*
-		* Switch default core markup for search form, comment form, and comments
-		* to output valid HTML5.
-		*/
-	add_theme_support(
-		'html5',
-		array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-			'style',
-			'script',
-		)
-	);
+    add_theme_support( 'customize-selective-refresh-widgets' );
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support(
-		'custom-background',
-		apply_filters(
-			'bressol_nl_custom_background_args',
-			array(
-				'default-color' => 'ffffff',
-				'default-image' => '',
-			)
-		)
-	);
-
-	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
-
-	/**
-	 * Add support for core custom logo.
-	 *
-	 * @link https://codex.wordpress.org/Theme_Logo
-	 */
-	add_theme_support(
-		'custom-logo',
-		array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		)
-	);
+    add_theme_support(
+        'custom-logo',
+        array(
+            'height'      => 250,
+            'width'       => 250,
+            'flex-width'  => true,
+            'flex-height' => true,
+        )
+    );
 }
 add_action( 'after_setup_theme', 'bressol_nl_setup' );
 
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
 function bressol_nl_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'bressol_nl_content_width', 640 );
+    $GLOBALS['content_width'] = apply_filters( 'bressol_nl_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'bressol_nl_content_width', 0 );
 
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
 function bressol_nl_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'bressol-nl' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'bressol-nl' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
+    register_sidebar(
+        array(
+            'name'          => esc_html__( 'Sidebar', 'bressol-nl' ),
+            'id'            => 'sidebar-1',
+            'description'   => esc_html__( 'Add widgets here.', 'bressol-nl' ),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</section>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        )
+    );
 }
 add_action( 'widgets_init', 'bressol_nl_widgets_init' );
 
-/**
- * Enqueue scripts and styles.
- */
 function bressol_nl_scripts() {
-	wp_enqueue_style( 'bressol-nl-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'bressol-nl-style', 'rtl', 'replace' );
+    wp_enqueue_style( 'bressol-nl-style', get_stylesheet_uri(), array(), _S_VERSION );
+    wp_style_add_data( 'bressol-nl-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'bressol-nl-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+    wp_enqueue_script( 'bressol-nl-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
 }
 add_action( 'wp_enqueue_scripts', 'bressol_nl_scripts' );
 
-/**
- * Implement the Custom Header feature.
- */
 require get_template_directory() . '/inc/custom-header.php';
-
-/**
- * Custom template tags for this theme.
- */
 require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Functions which enhance the theme by hooking into WordPress.
- */
 require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
 require get_template_directory() . '/inc/customizer.php';
 
-/**
- * Load Jetpack compatibility file.
- */
 if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
+    require get_template_directory() . '/inc/jetpack.php';
 }
 
-/**
- * Load WP Bootstrap Navwalker.
- */
 require_once get_template_directory() . '/inc/class-wp-bootstrap-navwalker.php';
 
 function mytheme_add_woocommerce_support() {
@@ -249,8 +177,6 @@ function handle_newsletter_subscription() {
 add_action('admin_post_newsletter_subscription', 'handle_newsletter_subscription');
 add_action('admin_post_nopriv_newsletter_subscription', 'handle_newsletter_subscription');
 
-
-
 // Función para crear la tabla personalizada
 function create_contacts_table() {
     global $wpdb;
@@ -282,10 +208,14 @@ function create_contacts_table() {
 }
 add_action('after_setup_theme', 'create_contacts_table');
 
+// Forzar a WooCommerce a usar plantillas del tema personalizado.
+function bressol_nl_woocommerce_template( $template, $template_name, $template_path ) {
+    $custom_template = get_stylesheet_directory() . '/woocommerce/' . $template_name;
 
+    if ( file_exists( $custom_template ) ) {
+        return $custom_template;
+    }
 
-
-
-
-
-
+    return $template;
+}
+add_filter( 'woocommerce_locate_template', 'bressol_nl_woocommerce_template', 10, 3 );
