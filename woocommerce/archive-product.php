d4494004328b4abdffd8cd13b88226e6c2415ff7
Ivan <ivanetbng@gmail.com>
2024-08-08 11:58:25 +0200

    Styled archive-product page with Bootstrap and added custom styles for add-to-cart button

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -5,15 +5,24 @@ get_header( 'shop' );
 
 echo '<div class="container">'; // Abrir contenedor Bootstrap
 
-do_action( 'woocommerce_before_main_content' );
-
+// Título de la página
+echo '<div class="row">';
+echo '<div class="col-12">';
 do_action( 'woocommerce_shop_loop_header' );
+echo '</div>';
+echo '</div>'; // Cerrar row para el título
 
 if ( woocommerce_product_loop() ) {
+
+    // Número de resultados y filtros
+    echo '<div class="row">';
+    echo '<div class="col-12">';
     do_action( 'woocommerce_before_shop_loop' );
+    echo '</div>';
+    echo '</div>'; // Cerrar row para resultados y filtros
 
-    echo '<div class="row">'; // Añadir row de Bootstrap
-
+    // Productos
+    echo '<div class="row">';
     woocommerce_product_loop_start();
 
     if ( wc_get_loop_prop( 'total' ) ) {
@@ -25,8 +34,7 @@ if ( woocommerce_product_loop() ) {
     }
 
     woocommerce_product_loop_end();
-
-    echo '</div>'; // Cerrar row de Bootstrap
+    echo '</div>'; // Cerrar row para productos
 
     do_action( 'woocommerce_after_shop_loop' );
 } else {
@@ -34,7 +42,6 @@ if ( woocommerce_product_loop() ) {
 }
 
 do_action( 'woocommerce_after_main_content' );
-
 do_action( 'woocommerce_sidebar' );
 
 echo '</div>'; // Cerrar contenedor Bootstrap
